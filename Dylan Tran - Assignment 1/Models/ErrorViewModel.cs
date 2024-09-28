7cbbeb2e93729f59914ab6b470b4f5e3e27cd876
Kneepads2 <dylant6649@gmail.com>
2024-09-28 16:18:07 -0400

    Add project files.

---
+++ Dylan Tran - Assignment 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dylan_Tran___Assignment_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
