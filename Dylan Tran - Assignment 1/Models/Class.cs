8772f19fccb5878c06517ea8159af487bac176a6
Kneepads2 <dylant6649@gmail.com>
2024-09-29 00:09:16 -0400

    Worked more on the request form, added validations for user entries

---
+++ Dylan Tran - Assignment 1/Models/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace Dylan_Tran___Assignment_1.Models
+{
+    public class Class
+    {
+    }
+}
--- Dylan Tran - Assignment 1/Models/RequestForm.cs
+++ Dylan Tran - Assignment 1/Models/RequestForm.cs
@@ -10,6 +10,8 @@ namespace Dylan_Tran___Assignment_1.Models
         public string? Role { get; set; }
         public string? Equipment { get; set; }
         public int? Duration { get; set; }
+        public string? Request_Details { get; set; }
+
     }
 }
 
